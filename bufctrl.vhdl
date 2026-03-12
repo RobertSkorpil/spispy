2a2303cf6f89952f2ac4b274d41cd6c8b742602a
Robert Skorpil <robert@skorpil.net>
2026-03-12 12:28:34 +0100

    polish

--- bufctrl.vhdl
+++ bufctrl.vhdl
@@ -30,58 +30,56 @@ entity BUFCTRL is
 end entity BUFCTRL;
 
 architecture RTL of BUFCTRL is
-    signal read_ptr : unsigned(8 downto 0) := (others => '0');
-    signal next_read_ptr: unsigned(8 downto 0) := (others => '0');
-    signal write_ptr : unsigned(8 downto 0) := (others => '0');
-    signal next_write_ptr: unsigned(8 downto 0) := (others => '0');
+    signal read_ptr_D, read_ptr_Q : unsigned(8 downto 0) := (others => '0');
+    signal write_ptr_D, write_ptr_Q: unsigned(8 downto 0) := (others => '0');
     signal overflow : std_logic := '0';
     signal set_overflow : std_logic := '0';
     signal clear_overflow : std_logic := '0';
 begin
-    CAP_COMB: process(CAP_ADDR, CAP_COUNT, CAP_STROBE, CAP_TIME, write_ptr, read_ptr)
-    variable new_write_ptr : unsigned(8 downto 0);
+    CAP_COMB: process(CAP_ADDR, CAP_COUNT, CAP_STROBE, CAP_TIME, write_ptr_Q, read_ptr_Q)
+    variable new_write_ptr_Q : unsigned(8 downto 0);
     begin
-        new_write_ptr := write_ptr + 1;
-        MEM_ADDR_OUT <= std_logic_vector(write_ptr);
+        new_write_ptr_Q := write_ptr_Q + 1;
+        MEM_ADDR_OUT <= std_logic_vector(write_ptr_Q);
         MEM_DATA_OUT <= CAP_ADDR & CAP_COUNT & CAP_TIME;
         set_overflow <= '0';
-        next_write_ptr <= write_ptr;
+        write_ptr_D <= write_ptr_Q;
         MEM_WRITE <= '0';
         if CAP_STROBE = '1' then
-            if new_write_ptr = read_ptr then
+            if new_write_ptr_Q = read_ptr_Q then
                 set_overflow <= '1';
             else
-                next_write_ptr <= new_write_ptr;
+                write_ptr_D <= new_write_ptr_Q;
                 MEM_WRITE <= '1';
             end if;
         end if;
     end process;
 
-    READ_IN_COMB: process(READ_NEXT, read_ptr, write_ptr)
-    variable new_read_ptr : unsigned(8 downto 0);
+    READ_IN_COMB: process(READ_NEXT, read_ptr_Q, write_ptr_Q)
+    variable new_read_ptr_Q : unsigned(8 downto 0);
     begin
-        new_read_ptr := read_ptr + 1;
-        next_read_ptr <= read_ptr;
+        new_read_ptr_Q := read_ptr_Q + 1;
+        read_ptr_D <= read_ptr_Q;
         if READ_CLEAR = '1' then
-            next_read_ptr <= write_ptr;
+            read_ptr_D <= write_ptr_Q;
             clear_overflow <= '1';
-        elsif READ_NEXT = '1' and read_ptr /= write_ptr then
+        elsif READ_NEXT = '1' and read_ptr_Q /= write_ptr_Q then
             clear_overflow <= '1';
-            next_read_ptr <= new_read_ptr;
+            read_ptr_D <= new_read_ptr_Q;
         else
             clear_overflow <= '0';
         end if;
     end process;
 
-    READ_OUT_COMB: process(MEM_DATA_IN, overflow, read_ptr, write_ptr)
+    READ_OUT_COMB: process(MEM_DATA_IN, overflow, read_ptr_Q, write_ptr_Q)
     begin
-        MEM_ADDR_IN <= std_logic_vector(read_ptr);
+        MEM_ADDR_IN <= std_logic_vector(read_ptr_Q);
 
         READ_ADDR <= MEM_DATA_IN(63 downto 40);
         READ_COUNT <= MEM_DATA_IN(39 downto 16);
         READ_TIME <= MEM_DATA_IN(15 downto 0);
         READ_LOST <= overflow;
-        if read_ptr = write_ptr then
+        if read_ptr_Q = write_ptr_Q then
             READ_READY <= '0';
         else
             READ_READY <= '1';
@@ -92,15 +90,16 @@ begin
     begin
         if rising_edge(CLK) then
             if RESET_N = '0' then
-                read_ptr <= (others => '0');
-                write_ptr <= (others => '0');
+                read_ptr_Q <= (others => '0');
+                write_ptr_Q <= (others => '0');
                 overflow <= '0';
             else
-                read_ptr <= next_read_ptr;
-                write_ptr <= next_write_ptr;
+                read_ptr_Q <= read_ptr_D;
+                write_ptr_Q <= write_ptr_D;
                 if clear_overflow = '1' then
                     overflow <= '0';
-                elsif set_overflow = '1' then
+                end if;
+                if set_overflow = '1' then
                     overflow <= '1';
                 end if;
             end if;
